ce56b6eb5ead60d1b06546c80b15a686b41703fd
northoutIshan <ishandeveloper02@gmail.com>
2017-06-17 20:35:47 +0530

    Attach server connection

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const UserClass = require('./user');
 const serverConnection = require('./serverConnection');
 // Create a server with a host and port
 const server = new Hapi.Server();  
-server.connection(serverConnection.dev);
+server.connection(serverConnection.devServer);
 
 
 const User = new UserClass();
